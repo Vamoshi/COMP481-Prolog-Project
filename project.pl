4a053b204ea7a7976821659b42c320de3d7511d4
Vamoshi <64162877+Vamoshi@users.noreply.github.com>
2024-12-08 20:31:14 -0800

    feat: determiner follows vowel/consonant
    - a/an depending on the starting letter of next word

--- project.pl
+++ project.pl
@@ -1,81 +1,34 @@
-% You are only required to create Prolog rules for the following grammar rules.
-% • Simple Sentences
-% • Noun Phrases
-% • Verb Phrases
-% • Prepositional Phrases
-% • Adjective Phrases
-% • Determiners
-% • Verbs
-% • Adjectives
-% • Nouns
-% • Pronouns
+% ------------------- Rules ------------------------
 
-% Sentence = Noun Phrase + Verb Phrase
-% Sentence = Noun Phrase + Verb Phrase + Preposition Phrase
-
-% Noun Phrase = Determiner + Noun
-%             = Pronoun
-%             = Determiner + Adjective Phrase + Noun
-
-% Verb Phrase = Verb + Noun Phrase
-%             = Verb
-%             = Verb + Prepositional Phrase
-
-% Prepositional Phrase = Preposition + Noun Phrase
-
-% Adjective Phrase = Adjective
-%                  = Adjective + Adjective + ... + Adjective
-
-% ------------------- RULES ------------------------
-
-% Sentence = Noun Phrase + Verb Phrase
-% Sentence = Noun Phrase + Verb Phrase + Preposition Phrase
 sentence --> noun_phrase(subject), verb_phrase.
 sentence --> noun_phrase(subject), verb_phrase, preposition_phrase.
 
-% Noun Phrase = Determiner + Noun
-%             = Pronoun
-%             = Determiner + Adjective Phrase + Noun
-noun_phrase(_) --> determiner, noun.
-% noun_phrase(_) --> determiner, adjective_phrase, noun.
-noun_phrase(_) --> determiner, adjective_phrase([]), noun.
+noun_phrase(_) --> determiner(Type), adjective_phrase(Type, []), noun(Type).
 noun_phrase(subject) --> pronoun(subject).
 noun_phrase(subject) --> pronoun(object).
 
-% Verb Phrase = Verb + Noun Phrase
-%             = Verb
-%             = Verb + Prepositional Phrase
 verb_phrase --> verb.
 verb_phrase --> verb, noun_phrase(object).
-verb_phrase --> verb, preposition_phrase.
 
-% Prepositional Phrase = Preposition + Noun Phrase
 preposition_phrase --> preposition, noun_phrase(object).
 
-% Adjective Phrase = Adjective
-%                  = Adjective1 + Adjective2 + ... + AdjectiveN (Unique values)
-adjective_phrase(_) --> [].
-adjective_phrase(Used) -->
-    % Bind current adjective to A
-    [A], 
+adjective_phrase(Type, Used) --> [].
+adjective_phrase(Type, Used) --> 
+    [Adjective], 
     { 
-        % For adjective A, 
-        % make sure it's a defined adjective
-        adjective(A), 
-        % make sure it's not a member of used adjectives
-        \+ member(A, Used) 
+        adjective((Adjective, Type)), 
+        \+ member(Adjective, Used) 
     },
-    % add A to list of Used adjectives
-    adjective_phrase([ A | Used ]).
+    adjective_phrase(Type, [Adjective | Used]).
 
 % ------------------- Lexicon ------------------------
 
 % Determiners
-determiner --> [the].
-determiner --> [a].
-determiner --> [an].
-determiner --> [this].
-determiner --> [these].
+determiner(_) --> [the].
+determiner(_) --> [this].
+determiner(_) --> [these].
+determiner(vowel) --> [an].
+determiner(consonant) --> [a].
 
 % Pronouns
 pronoun(subject) --> [he].
@@ -86,28 +39,27 @@ pronoun(object) --> [her].
 pronoun(object) --> [them].
 
 % Nouns
-noun --> [man].
-noun --> [airplane].
-
-% Verbs
-verb --> [eats].
-verb --> [ate].
-verb --> [thinks].
-verb --> [thought].
-verb --> [rides].
-verb --> [rode].
+noun(consonant) --> [man].
+noun(consonant) --> [men].
+noun(vowel) --> [airplane].
+noun(vowel) --> [airplanes].
 
 % Adjectives
-adjective(big).
-adjective(happy).
+adjective((ugly, vowel)).
+adjective((happy, consonant)).
+
+% Verbs
+verb --> [ride].
+verb --> [rides].
 
 % Prepositions
+preposition --> [in].
+preposition --> [with].
 preposition --> [to].
 preposition --> [from].
-preposition --> [with].
 preposition --> [on].
-preposition --> [in].
 preposition --> [at].
+preposition --> [by].
 
 output_to_file :-
     open('output.txt', write, Stream),
